36e3d5d068f9404e5fbcebfffcff3df52bddcec8
Anton Shipitsin <antonshipitsin@gmail.com>
2022-01-11 19:40:47 +0600

    feat: empliment requested function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (arr) {
+if (arr == undefined) return []
+  let arr2 = []
+    for (let i = 0; i < arr.length; i++) {
+        if (i % 2 == 0) {
+            arr2 = arr2.concat(arr[i])
+        }
+        else {
+            arr2 = arr2.concat(arr[i].reverse())
+        }
+    }
+    return arr2
 }
